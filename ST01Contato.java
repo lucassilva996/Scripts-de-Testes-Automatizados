15e04b282cf0418eed585a3a9ab172c730f64834
Lucas S Lima <31423863+lucassilva996@users.noreply.github.com>
2020-08-03 14:37:37 -0300

    Add files via upload

---
+++ ST01Contato.java
@@ -0,0 +1,22 @@
+<!DOCTYPE suite SYSTEM "http://testng.org/testng-1.0.dtd" >
+<suite name="ST01Contato" verbose="1" >
+
+  <test name="com.example.tests.CT01ValidarLayout" >
+    <classes>
+      <class name="com.example.tests.CT01ValidarLayout" />
+    </classes>
+  </test>
+
+  <test name="com.example.tests.CT02ValidarCriticaCamposObrigatorios" >
+    <classes>
+      <class name="com.example.tests.CT02ValidarCriticaCamposObrigatorios" />
+    </classes>
+  </test>
+
+  <test name="com.example.tests.CT03EnviarMensagem" >
+    <classes>
+      <class name="com.example.tests.CT03EnviarMensagem" />
+    </classes>
+  </test>
+
+</suite>
